884664b996b81a2f032a9cb8a378a0df3f2727c1
Jeremy Maia <jmaia@student.42.fr>
2022-01-25 17:15:24 +0100

    Create init_signal_handling()

--- srcs/server/handle_messages.c
+++ srcs/server/handle_messages.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 18:06:54 by jmaia             #+#    #+#             */
-/*   Updated: 2022/01/25 16:13:54 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/01/25 17:12:49 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
+static void	init_signal_handling(void);
 static void	handle_signal(int sig, siginfo_t *info, void *ucontext);
 
+int	g_sig = 0;
+
 int	handle_messages(void)
+{
+	char	*message;
+
+	init_signal_handling();
+	while (1)
+		pause();
+	return (1);
+}
+
+static void	init_signal_handling(void)
 {
 	struct sigaction	sa;
 	sigset_t			set;
@@ -30,14 +43,11 @@ int	handle_messages(void)
 	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, 0);
 	sigaction(SIGUSR2, &sa, 0);
-	while (1)
-		pause();
-	return (1);
 }
 
 static void	handle_signal(int sig, siginfo_t *info, void *ucontext)
 {
 	(void) info;
 	(void) ucontext;
-	printf("Signal : %d\n", sig);
+	g_sig = sig;
 }
